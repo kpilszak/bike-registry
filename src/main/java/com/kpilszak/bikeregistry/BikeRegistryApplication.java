aeef9638f43d445a09df982ce7466ea49972278f
kpilszak <klaudia.pilszak@gmail.com>
2020-07-06 07:37:29 +0200

    Create repo

---
+++ src/main/java/com/kpilszak/bikeregistry/BikeRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.bikeregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpilszak/bikeregistry/BikeRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.bikeregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
