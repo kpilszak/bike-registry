19a1e4a9f74aecaf82cd66adf9c955cd5ae7acdc
kpilszak <klaudia.pilszak@gmail.com>
2020-07-07 08:37:56 +0200

    Add bike model

--- src/main/java/com/kpilszak/bikeregistry/BikeRegistryApplication.java
+++ src/main/java/com/kpilszak/bikeregistry/BikeRegistryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BikeRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BikeRegistryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BikeRegistryApplication.class, args);
+    }
 
 }
